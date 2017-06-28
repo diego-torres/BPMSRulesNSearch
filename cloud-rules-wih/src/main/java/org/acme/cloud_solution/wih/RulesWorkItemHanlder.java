e71886850295e3dc72e30329b135bb053e3659f4
diego-torres <dtorresf@redhat.com>
2017-06-28 19:18:03 -0400

    setting proper package for rules

--- cloud-rules-wih/src/main/java/org/acme/cloud_solution/wih/RulesWorkItemHanlder.java
+++ cloud-rules-wih/src/main/java/org/acme/cloud_solution/wih/RulesWorkItemHanlder.java
@@ -1,66 +1,91 @@
 package org.acme.cloud_solution.wih;
 
+import java.util.Collection;
+
 import org.drools.compiler.kproject.ReleaseIdImpl;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieScanner;
 import org.kie.api.builder.ReleaseId;
+import org.kie.api.definition.KiePackage;
+import org.kie.api.definition.rule.Rule;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
 import org.kie.internal.runtime.Cacheable;
+import org.kie.api.runtime.process.ProcessInstance;
 
 public class RulesWorkItemHanlder implements WorkItemHandler, Cacheable {
-    
-    private ReleaseId releaseId;
-    private KieContainer kieContainer;
-    private KieScanner kieScanner;
-    
-    public RulesWorkItemHanlder(String releaseIdStr) {
-        releaseId = new ReleaseIdImpl(releaseIdStr);
-        KieServices ks = KieServices.Factory.get();
-        kieContainer = ks.newKieContainer(releaseId);
-        kieScanner = ks.newKieScanner(kieContainer);
-        kieScanner.start(10000L);
-        System.out.println("Started KieScanner for releaseId : " + releaseIdStr);
-    }
 
-    public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
-        
-        
-        System.out.println("Executing rules for releaseId " + releaseId); 
-        KieSession kieSession;
-        String kieSessionName = (String)workItem.getParameter("kieSession");
-        if (kieSessionName == null || kieSessionName.isEmpty()) {
-            System.out.println("***** WARNING ********:  value for servicetask parameter kieSession is not found!!!!  Check the data inputs assignments for your service task");
-            kieSession = kieContainer.newKieSession();
-        } else {
-            kieSession = kieContainer.newKieSession(kieSessionName);
-        }
-        
-        for (String param : workItem.getParameters().keySet()) {
-            if (param.startsWith("fact_")) {
-                kieSession.insert(workItem.getParameter(param));
-            }
-        }
-        String processId = (String)workItem.getParameter("processId");
-        if (processId != null && !processId.isEmpty()) {
-            kieSession.startProcess(processId);
-        }
-        kieSession.fireAllRules();
-        kieSession.dispose();
-        manager.completeWorkItem(workItem.getId(), workItem.getParameters());         
-    }
+	private ReleaseId releaseId;
+	private KieContainer kieContainer;
+	private KieScanner kieScanner;
 
-    public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
-        // TODO Auto-generated method stub
-        
-    }
+	public RulesWorkItemHanlder(String releaseIdStr) {
+		releaseId = new ReleaseIdImpl(releaseIdStr);
+		KieServices ks = KieServices.Factory.get();
+		kieContainer = ks.newKieContainer(releaseId);
 
-    public void close() {
-        kieScanner.stop(); 
-        
-    }
+		// Iterate and print list of rules loaded in KieBase
+		Collection<KiePackage> kPackages = kieContainer.getKieBase().getKiePackages();
+		for (KiePackage kPackage : kPackages) {
+			Collection<Rule> rules = kPackage.getRules();
+			for (Rule rule : rules) {
+				System.out.println(kPackage.getName() + " : Following rule found: " + rule.getName());
+			}
+		}
+
+		kieScanner = ks.newKieScanner(kieContainer);
+		kieScanner.start(10000L);
+	}
+
+	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+
+		String kieSessionName = (String) workItem.getParameter("kieSession");
+		String processId = (String) workItem.getParameter("processId");
+
+		StringBuilder sBuilder = new StringBuilder("executeWorkItem() \n\tExecuting rules for releaseId " + releaseId);
+		sBuilder.append("\n\tkieSessionName = " + kieSessionName);
+		sBuilder.append("\n\tprocessId = " + processId);
+		System.out.println(sBuilder.toString());
+
+		KieSession kieSession;
+		if (kieSessionName == null || kieSessionName.isEmpty()) {
+			System.out.println("kieSession not found, creating a new one");
+			kieSession = kieContainer.newKieSession();
+		} else {
+			System.out.println("using available kieSession");
+			kieSession = kieContainer.newKieSession(kieSessionName);
+		}
+
+		for (String param : workItem.getParameters().keySet()) {
+			if (param.startsWith("fact_")) {
+				System.out.println("setting parameter: " + param + " with value: " + workItem.getParameter(param));
+				kieSession.insert(workItem.getParameter(param));
+			}
+		}
+		if (processId != null && !processId.isEmpty()) {
+			ProcessInstance pInstance = kieSession.startProcess(processId);
+			if (pInstance == null)
+				throw new RuntimeException("executeWorkItem() pInstance not created with processId = " + processId);
+		}
+
+		int numRulesFired = kieSession.fireAllRules();
+		System.out.println("\nNumber of rules fired = " + numRulesFired);
+
+		kieSession.dispose();
+		manager.completeWorkItem(workItem.getId(), workItem.getParameters());
+	}
+
+	public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void close() {
+		kieScanner.stop();
+
+	}
 
 }
