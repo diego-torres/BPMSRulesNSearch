836df4bc734f7c0282ba19d6c244d969d1afb59e
diego-torres <dtorresf@redhat.com>
2017-07-13 08:39:45 -0400

    clean up unused assets and remove additional debugging prints not required now

--- cloud-rules-wih/src/main/java/org/acme/cloud_solution/wih/RulesWorkItemHanlder.java
+++ cloud-rules-wih/src/main/java/org/acme/cloud_solution/wih/RulesWorkItemHanlder.java
@@ -1,15 +1,9 @@
 package org.acme.cloud_solution.wih;
 
-import java.util.Collection;
-
 import org.drools.compiler.kproject.ReleaseIdImpl;
-import org.drools.core.event.DebugAgendaEventListener;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieScanner;
 import org.kie.api.builder.ReleaseId;
-import org.kie.api.definition.KiePackage;
-import org.kie.api.definition.rule.Rule;
-import org.kie.api.event.rule.DebugRuleRuntimeEventListener;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.process.ProcessInstance;
@@ -29,44 +23,26 @@ public class RulesWorkItemHanlder implements WorkItemHandler, Cacheable {
 		KieServices ks = KieServices.Factory.get();
 		kieContainer = ks.newKieContainer(releaseId);
 
-		// Iterate and print list of rules loaded in KieBase
-		Collection<KiePackage> kPackages = kieContainer.getKieBase().getKiePackages();
-		for (KiePackage kPackage : kPackages) {
-			Collection<Rule> rules = kPackage.getRules();
-			for (Rule rule : rules) {
-				System.out.println(kPackage.getName() + " : Following rule found: " + rule.getName());
-			}
-		}
-
 		kieScanner = ks.newKieScanner(kieContainer);
 		kieScanner.start(10000L);
 	}
 
 	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
-
 		String kieSessionName = (String) workItem.getParameter("kieSession");
 		String processId = (String) workItem.getParameter("processId");
 
-		StringBuilder sBuilder = new StringBuilder("executeWorkItem() \n\tExecuting rules for releaseId " + releaseId);
-		sBuilder.append("\n\tkieSessionName = " + kieSessionName);
-		sBuilder.append("\n\tprocessId = " + processId);
-		System.out.println(sBuilder.toString());
-
 		KieSession kieSession;
 		if (kieSessionName == null || kieSessionName.isEmpty()) {
-			System.out.println("kieSession not found, creating a new one");
 			kieSession = kieContainer.newKieSession();
 		} else {
-			System.out.println("using available kieSession");
 			kieSession = kieContainer.newKieSession(kieSessionName);
 		}
-		
-		kieSession.addEventListener(new DebugAgendaEventListener());
-		kieSession.addEventListener(new DebugRuleRuntimeEventListener());
+
+		// kieSession.addEventListener(new DebugAgendaEventListener());
+		// kieSession.addEventListener(new DebugRuleRuntimeEventListener());
 
 		for (String param : workItem.getParameters().keySet()) {
 			if (param.startsWith("fact_")) {
-				System.out.println("setting parameter: " + param + " with value: " + workItem.getParameter(param));
 				kieSession.insert(workItem.getParameter(param));
 			}
 		}
@@ -76,9 +52,7 @@ public class RulesWorkItemHanlder implements WorkItemHandler, Cacheable {
 				throw new RuntimeException("executeWorkItem() pInstance not created with processId = " + processId);
 		}
 
-		int numRulesFired = kieSession.fireAllRules();
-		System.out.println("\nNumber of rules fired = " + numRulesFired);
-
+		kieSession.fireAllRules();
 		kieSession.dispose();
 		manager.completeWorkItem(workItem.getId(), workItem.getParameters());
 	}
